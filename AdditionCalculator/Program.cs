cc64d64d8183b1cebc699d2c80f874cdf2e63a63
Bibhot1 <bndazbvu.me12@gmail.com>
2024-05-12 07:10:24 +1200

    Add project files.

---
+++ AdditionCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("please entere the first number");
+int num1 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("please enter the second number");
+int num2 = Int32.Parse(Console.ReadLine());
+
+int result = num1 + num2;
+Console.WriteLine("the result of num1 and num2 is" + result);
+
+
